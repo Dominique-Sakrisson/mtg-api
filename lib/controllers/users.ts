68da4f72247eb7541012b0e0266205170edee1de
Dominique Sakrisson <domsakrisson@gmailcom>
2022-01-10 17:00:14 -0800

    Add module for generating jsonwebtoken

--- lib/controllers/users.ts
+++
@@ -1,7 +0,0 @@
-import {Router} from 'express'
-
-module.exports = Router()
-.get('/', async (req,res,next) =>{
-    console.log('hello from "/" ');
-    res.send('<h1>hello there</h1>')
-})
--- lib/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-
-const app: express.Application = express();
-
-app.use('/api/v1/users', require('./controllers/users'))
-
-export default app;
